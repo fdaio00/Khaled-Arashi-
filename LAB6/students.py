fb55af9a280b0e81b15deca022d0600cd07d16c0
Khaled Jalal Al-arashi <77218749+kh-cs@users.noreply.github.com>
2024-01-10 16:45:58 +0300

    Update students.py

--- LAB6/students.py
+++ LAB6/students.py
@@ -1,5 +1,3 @@
-import os
-
 # function to insert data.
 def add(name , age , major , avg):        
     try:
@@ -19,16 +17,12 @@ def add(name , age , major , avg):
 def read():
     f = open("first.txt", "r")
     for i in f :
-        f2.write(i)
         print('Name    : ',i)
         i = f.readline()
-        f2.write(i)
         print('Age     : ',i)
         i = f.readline()
-        f2.write(i)
         print('Major   : ',i)
         i = f.readline()
-        f2.write(i)
         print('Average : ',i)
         print('----------------------')
     f.close()
@@ -38,9 +32,7 @@ while True :
     c = int(input('''
 1 ) Add Student.
 2 ) Read Student Data.
-3 ) Search.
-4 ) Update Data.
-5 ) exit.
+3 ) exit.
 '''))
     print('===============================')
     if c==1:
@@ -52,10 +44,7 @@ while True :
     elif c==2:
         read()
     elif c==3:
-#         name = input('Enter the name : ')
-        search('khaled s al-arashi')
-    elif c==4:
-        update('khaled jalal al-arashi')
-    elif c==5:
         print('exit....')
         break
+    else:
+        print('error...')
